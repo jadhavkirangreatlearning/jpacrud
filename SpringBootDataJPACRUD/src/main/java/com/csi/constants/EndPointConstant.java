ba497bcac7c6cd6a41c1cca2921bb7cd3d0a12e1
jadhavkirangreatlearning <jadhavkiran.greatlearning@gmail.com>
2023-03-09 10:19:41 +0530

    initial commit

---
+++ SpringBootDataJPACRUD/src/main/java/com/csi/constants/EndPointConstant.java
@@ -0,0 +1,8 @@
+package com.csi.constants;
+
+public class EndPointConstant {
+
+    public static final String SAVE_DATA="/savedata";
+
+    public static final String GET_ALL_DATA="/getalldata";
+}
